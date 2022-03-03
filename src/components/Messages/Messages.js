a207d8d55a07bd988e9486af7c2a0a48e48c0c8c
Renat <restspread@gmail.com>
2022-03-03 23:52:10 +0300

    refactor

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,30 +1,39 @@
 import { NavLink } from "react-router-dom";
 import style from "./Messages.module.css";
 
+const DialogItem = (props) => {
+	const id = `/messages/${props.id}`;
+	return (
+		<div className={style.dialog}>
+			<NavLink to={id} className={style.dialog}>{props.name}</NavLink>
+		</div>
+	)
+}
+
+const MessageItem = (props) => {
+	return (
+		<div className={style.message}>{props.message}</div>
+	)
+}
+
+
+
 const Messages = () => {
 	return (
 		<div className={style.container}>
 			<div className={style.messagesTop}>Messages</div>
 			<div className={style.dialogsTop}>John Doe</div>
 			<div className={style.dialogs}>
-				<div className={style.dialog}>
-					<NavLink to="/messages/user1" className={style.dialog}>Name 1</NavLink>
-				</div>
-				<div className={style.dialog}>
-					<NavLink to="/messages/user2" className={style.dialog}>Name 2</NavLink>
-				</div>
-				<div className={style.dialog}>
-					<NavLink to="/messages/user3" className={style.dialog}>Name 3</NavLink>
-				</div>
-				<div className={style.dialog}>
-					<NavLink to="/messages/user4" className={style.dialog}>Name 4</NavLink>
-				</div>
+				<DialogItem name="Name 1" id="user1" />
+				<DialogItem name="Name 2" id="user2" />
+				<DialogItem name="Name 3" id="user3" />
+				<DialogItem name="Name 4" id="user4" />
 			</div>
 			<div className={style.messages}>
-				<div className={style.message}>message 1</div>
-				<div className={style.message}>message 2</div>
-				<div className={style.message}>message 3</div>
-				<div className={style.message}>message 4</div>
+				<MessageItem message="message 1" />
+				<MessageItem message="message 2" />
+				<MessageItem message="message 3" />
+				<MessageItem message="message 4" />
 			</div>
 			<div className={style.dialogsBottom}>Type a message here</div>
 		</div>
