2b7ced54dc30fcf9d3475b0d8cdbe887d221d61e
Renat <restspread@gmail.com>
2022-03-21 22:48:49 +0300

    add Follow component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
+import Follow from "./Follow/Follow";
 import Friends from "./Friends/Friends";
 import style from "./Home.module.css";
 
 const Home = () => {
 	return(
 		<div className={style.container}>
-			<div className={style.column1}></div>
+			<div className={style.column1}>
+				<Follow />
+			</div>
 			<div className={style.column2}></div>
 			<div className={style.column3}>
 				<Friends />
