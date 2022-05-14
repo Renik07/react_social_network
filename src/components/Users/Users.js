5a9705c46bc3c21103a2fda725ba77cdcb74794e
Renat <restspread@gmail.com>
2022-05-14 21:13:33 +0300

    refactor UsersContainer

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,25 +1,56 @@
 import React from 'react';
 import style from './Users.module.css';
-import * as axios from 'axios';
 import userAvatar from '../../assets/images/avatar.png';
 
+const Users = (props) => {
+
+	let pages = Math.ceil(props.totalUsersCount / props.pageSize);
+	let arrPages = [];
+
+	for (let i = 1; i <= pages; i++) {
+		arrPages.push(i);
+	}
+
+	let curP = props.currentPage;
+	let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+	let curPL = curP + 4;
+	let slicedPages = arrPages.slice( curPF, curPL);
+
+	return (
+		<div className={style.container}>
+			<h2 className={style.title}>Profiles</h2>
+			<div>
+				{ slicedPages.map(p => {
+					return (
+						<span className={props.currentPage === p ? style.selectedPage : style.buttonPage} onClick={ () => {props.onPageChanged(p)} }>{ p } </span>
+					)
+				}) }
+			</div>
+			<div className={style.wrapper}>
+				{ 
+					props.users.map(user => <div className={style.userWrapper} key={user.id}>				
+							<img className={style.image} src={ user.photos.small != null ? user.photos.small : userAvatar } alt="" />
+							<h3 className={style.fullName}>{ user.name }</h3>
+							<h4 className={style.profession}>{ user.profession }</h4>
+							{ user.followed 
+								? <button className={style.buttonUnfollow} onClick={ () => {props.unfollow(user.id)} }>Unfollow</button> 
+								: <button className={style.buttonFollow} onClick={ () => {props.follow(user.id)} }>Follow</button> 
+							}
+					</div>
+					) 
+				}
+			</div>
+		</div>
+	)
+}
+
+export default Users;
+
+
+/* 
+
 class Users extends React.Component {
-	componentDidMount() {
-		axios
-			.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-			.then(response => {
-				this.props.setUsers(response.data.items);
-				this.props.setTotalUsersCount(response.data.totalCount);
-			})
-	}
 
-	onPageChanged(pageNumber) {
-		this.props.setCurrentPage(pageNumber)
-
-		axios
-		.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-			.then(response => this.props.setUsers(response.data.items))
-	}
 
 	render() {
 
@@ -64,4 +95,4 @@ class Users extends React.Component {
 	}
 }
 
-export default Users;
+export default Users; */
