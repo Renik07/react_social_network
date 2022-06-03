19ebce0da87e301251e0a7a20d86f4009ac86ff0
Renat <restspread@gmail.com>
2022-06-03 22:14:17 +0300

    add component Feed

---
+++ src/components/Posts/Feed/Feed.js
@@ -0,0 +1,65 @@
+import style from "./Feed.module.css";
+import bookmark from "../../../assets/images/Feed/bookmark.png";
+import message from "../../../assets/images/Feed/message.png";
+
+const Feed = () => {
+  return (
+    <div className={style.feed}>
+      <div className={style.card}>
+        <div className={style.top}>
+          <div className={style.header}>
+            <div className={style.headerRow1}>
+              <div className={style.avatar}></div>
+              {/* <img className={style.avatar} src="" alt="" /> */}
+              <div className={style.nameTime}>
+                <div className={style.name}>John Doe</div>
+                <div className={style.time}>3 min ago</div>
+              </div>
+              <div className={style.iconMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div className={style.headerRow2}>
+              <div className={style.categoryLocation}>
+                <div className={style.category}>Epic Coder</div>
+                <div className={style.location}>Russia</div>
+              </div>
+              <div className={style.icons}>
+                <img className={style.iconBookmark} src={bookmark} alt="" />
+                <img className={style.iconMessage} src={message} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className={style.main}>
+            <h3 className={style.mainTitle}>Senior Wordpress Developer</h3>
+            <div className={style.fulltimePrice}>
+              <span className={style.mainFulltime}>Full Time</span>
+              <span className={style.mainPrice}>$30 / hr</span>
+            </div>
+            <p className={style.mainDesc}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id
+              magna sit amet.
+            </p>
+						<div className={style.mainSkiils}>
+							<div className={style.mainSkill}>HTML</div>
+							<div className={style.mainSkill}>PHP</div>
+							<div className={style.mainSkill}>CSS</div>
+							<div className={style.mainSkill}>Javascript</div>
+							<div className={style.mainSkill}>Wordpress</div>
+						</div>
+          </div>
+        </div>
+        <div className={style.bottom}>
+          <div className={style.like}>Like</div>
+          <div className={style.comments}>Comments</div>
+          <div className={style.views}>Views 50</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
--- src/components/Posts/ProfileTabs/ProfileTabs.js
+++ src/components/Posts/ProfileTabs/ProfileTabs.js
@@ -1,21 +1,50 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './ProfileTabs.css';
+import feed from '../../../assets/images/ProfileTabs/icon-feed.png';
+import info from '../../../assets/images/ProfileTabs/icon-info.png';
+import jobs from '../../../assets/images/ProfileTabs/icon-jobs.png';
+import bids from '../../../assets/images/ProfileTabs/icon-bids.png';
+import portfolio from '../../../assets/images/ProfileTabs/icon-portfolio.png';
+import reviews from '../../../assets/images/ProfileTabs/icon-reviews.png';
+import payment from '../../../assets/images/ProfileTabs/icon-payment.png';
+import Feed from '../Feed/Feed';
 
 const ProfileTabs = () => {
 	return (
 		<Tabs>
-			<TabList className='react-tabs__tab-list'>
-				<Tab>Feed</Tab>
-				<Tab>Info</Tab>
-				<Tab>Jobs</Tab>
-				<Tab>Bids</Tab>
-				<Tab>Portfolio</Tab>
-				<Tab>Reviews</Tab>
-				<Tab>Payment</Tab>
+			<TabList>
+				<Tab>
+					<img src={feed} alt="feed" />
+					<div>Feed</div>
+				</Tab>
+				<Tab>
+					<img src={info} alt="info" />
+					<div>Info</div>					
+					</Tab>
+				<Tab>
+					<img src={jobs} alt="jobs" />
+					<div>Jobs</div>					
+					</Tab>
+				<Tab>
+					<img src={bids} alt="bids" />
+					<div>Bids</div>					
+					</Tab>
+				<Tab>
+					<img src={portfolio} alt="portfolio" />
+					<div>Portfolio</div>					
+					</Tab>
+				<Tab>
+					<img src={reviews} alt="reviews" />
+					<div>Reviews</div>					
+					</Tab>
+				<Tab>
+					<img src={payment} alt="payment" />
+					<div>Payment</div>					
+					</Tab>
 			</TabList>
 
 			<TabPanel>
-				<h2>Any content 1</h2>
+				<Feed />
 			</TabPanel>
 			<TabPanel>
 				<h2>Any content 2</h2>
