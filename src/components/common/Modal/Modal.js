34be5bec0d5f5a6207487fca75fe385b0c17c951
Renat <restspread@gmail.com>
2022-08-04 00:00:36 +0300

    fix Modal

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -2,12 +2,17 @@ import style from "./Modal.module.css";
 
 const Modal = ({active, setActive, children}) => {
 	return (
-		<section className={active ? style.modalActive : style.modal} onClick={() => setActive(false)}>
-			<div className={style.modalWrapper} onClick={e => e.stopPropagation()}>
-				<div className={style.modalContent}>
-					{ children }
+		<section className={style.modalWindow}>
+			<div className={active ? style.outerBlock : style.outer} onClick={e => e.stopPropagation()}>
+				<div className={style.box}>
+					<div className={style.inner}>
+						<div className={style.content}>
+							{ children }
+							<div className={style.close} onClick={() => setActive(false)}>&#x2715;</div>
+						</div>
+					</div>
+					<div className={style.overlay} onClick={() => setActive(false)}></div>
 				</div>
-				<div className={style.close} onClick={() => setActive(false)}>&#x2715;</div>
 			</div>
 		</section>
 	)
